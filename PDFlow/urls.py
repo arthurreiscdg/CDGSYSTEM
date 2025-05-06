8eff89f5e24687ac7531dde3b0254ff9c0310b42
arthurreiscdg <arthur.casadagrafica@gmail.com>
2025-05-06 19:39:56 -0300

    Atualiza a URL raiz para redirecionar para a página de login e melhora a legibilidade do código

--- PDFlow/urls.py
+++ PDFlow/urls.py
@@ -2,13 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('formularios/', include('apps.formularios.urls')),
     path('usuarios/', include('apps.usuarios.urls')),
     path('webhooks/', include('apps.webhooks.urls')),
-    path('', include('apps.principal.urls')),
+    path('home/', include('apps.principal.urls')),
+    # Redirecionar a página raiz para a página de login
+    path('', RedirectView.as_view(url='/usuarios/login/', permanent=False)),
 ]
 
 if settings.DEBUG:
